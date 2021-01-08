d94093cce936948522df4c285c410156341c528e
Shashikanth G K <shashikanthgk99@gmail.com>
2021-01-09 01:54:38 +0530

    generated deployables

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,10 +5,8 @@ import {AnimalListComponent} from "./src/animal-list/animal-list.component"
 import {AnimalSelectionComponent} from "./src/animal-selection/animal-selection.component"
 import {ResultComponent} from "./src/result/result.component"
 const routes: Routes = [
+   {path: '', redirectTo: 'home', pathMatch: 'full'},
    { path: 'home', component: HomeComponent },
-   { path: 'animals-list', component: AnimalListComponent },
-   { path: 'animals-select', component: AnimalSelectionComponent },
-   { path: 'result', component: ResultComponent }
   ];
 
 @NgModule({
